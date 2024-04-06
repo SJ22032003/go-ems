c0ce404de811f9c1e955defcd65d79822e2d0f8b
SJ22032003 <jaryalshaurya2203@gmail.com>
2024-04-06 14:13:26 +0530

    Update validation rules for email and password in User model

--- models/user.go
+++ models/user.go
@@ -3,8 +3,8 @@ package model
 type User struct {
 	ID       int64  `json:"id"`
 	Name     string `binding:"required" json:"name"`
-	Email    string `binding:"required" json:"email"`
-	Password string `binding:"required" json:"password"`
+	Email    string `binding:"required,email" json:"email"`
+	Password string `binding:"gte=2,lte=10" json:"password"`
 }
 
 func (u *User) UserDetailsWithoutPassword() map[string]interface{} {
